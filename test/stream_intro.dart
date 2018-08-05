8c312fb70d7040d67f30d920314d18f8842f3d84
escamoteur <github@burkharts.net>
2018-08-05 14:16:10 +0200

    small changes

--- test/stream_intro.dart
+++ test/stream_intro.dart
@@ -2,31 +2,58 @@ import 'dart:async';
 
 import 'package:test/test.dart';
 
-
-
 void main() {
-   test('Create a Stream and add some values to it', () {
+  test('Create a Stream and add some values to it', () {
+    var controller = new StreamController<String>();
 
-        var controller = new StreamController<String>();
+    controller.add("Item1");
+  });
 
-        controller.add("Item1");
+  test('Setting the trap ', () async {
+    var controller = new StreamController<String>();
 
-   });
+    controller.stream.listen((item) => print(item)); // This is the Trap
 
-   test('Setting the trap ', () async {
+    controller.add("Item1");
+    controller.add("Item2");
+    controller.add("Item3");
 
-        var controller = new StreamController<String>();
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(milliseconds: 500));
+  });
 
+  test('Cancelling a subscription', () async {
+    var controller = new StreamController<String>();
+
+    StreamSubscription subscription =
         controller.stream.listen((item) => print(item)); // This is the Trap
 
-        controller.add("Item1");
-        controller.add("Item2");
-        controller.add("Item3");
+    controller.add("Item1");
+    controller.add("Item2");
+    controller.add("Item3");
 
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(milliseconds: 500));
 
-
-        // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
-        await Future.delayed(Duration(milliseconds: 500));
-   });
-
+    subscription.cancel;
+  });
 }
+
+// void myPrint(String message) {
+//   print(message);
+// }
+
+// void listenExample1() {
+//   var controller = new StreamController<String>();
+
+// //    StreamSubscription subscription = controller.stream.listen((item) => print(item)); // using a lambda function
+
+// //    StreamSubscription subscription = controller.stream.listen((item) => myPrint(item)); // using a lambda function
+
+// //    StreamSubscription subscription = controller.stream.listen(myPrint); // using tear-off
+
+//   StreamSubscription subscription = controller.stream.listen((item) {
+//     print(item);
+//     print(item.toUpperCase);
+//   }); // using tear-off
+// }
