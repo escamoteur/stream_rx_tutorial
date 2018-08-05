0d2b461d76e8cb8f231091cfcc5a6d94f5b54a5b
escamoteur <github@burkharts.net>
2018-08-05 18:48:13 +0200

    added map

--- test/rx_magic.dart
+++ test/rx_magic.dart
@@ -3,15 +3,115 @@ import 'package:rxdart/rxdart.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('Create Timer Observable', () async{
+  test('Create Observable from Stream', () async {
+    var controller = new StreamController<String>();
 
-      var timerObservable = Observable.periodic(Duration(seconds: 1), (x)=>x);
- 
-      timerObservable.listen(print);
+    var streamObservable = new Observable(controller.stream);
+
+    streamObservable.listen(print);
+
+    controller.add("Item1");
+    controller.add("Item2");
+    controller.add("Item3");
+
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(seconds: 5));
+  });
+
+  test('Create Timer Observable', () async {
+    var timerObservable = Observable.periodic(Duration(seconds: 1), (x) => x.toString());
+
+    timerObservable.listen(print);
+
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(seconds: 5));
+  });
+
+  Future<String> asyncFunction() async {
+    return Future.delayed(const Duration(seconds: 1), () => "AsyncRsult");
+  }
+
+  test('Create Observable from Future', () async {
+    print('start');
+
+    var fromFutureObservable = Observable.fromFuture(asyncFunction());
+
+    fromFutureObservable.listen(print);
+
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(seconds: 5));
+  });
+
+  test('PublishSubject', () async {
+    var subject = new PublishSubject<String>();
+
+    subject.listen((item) => print(item));
+
+    subject.add("Item1");
+
+    subject.listen((item) => print(item.toUpperCase()));
+
+    subject.add("Item2");
+    subject.add("Item3");
 
     // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
     await Future.delayed(Duration(seconds: 5));
 
+    // This will cancel all Subscriptions
+    subject.close;
   });
 
+  test('BehaviourSubject', () async {
+    var subject = new BehaviorSubject<String>();
+
+    subject.listen((item) => print(item));
+
+    subject.add("Item1");
+
+    subject.add("Item2");
+
+    subject.listen((item) => print(item.toUpperCase()));
+
+    subject.add("Item3");
+
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(seconds: 5));
+
+    // This will cancel all Subscriptions
+    subject.close;
+  });
+
+  test('Map toUpper', () async {
+    var subject = new PublishSubject<String>();
+
+    subject.map((item) => item.toUpperCase()).listen(print);
+
+    subject.add("Item1");
+    subject.add("Item2");
+    subject.add("Item3");
+
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(seconds: 5));
+
+    // This will cancel all Subscriptions
+    subject.close;
+  });
+
+  test('Map toUpper from int', () async {
+    var subject = new PublishSubject<int>();
+
+    subject.map((intValue) => intValue.toString())
+      .map((item) => item.toUpperCase())
+        .listen(print);
+
+    subject.add(1);
+    subject.add(2);
+    subject.add(3);
+
+    // this is only to prevent the testing framework to kill this process before all items on the Stream are processed
+    await Future.delayed(Duration(seconds: 5));
+
+    // This will cancel all Subscriptions
+    subject.close;
+  });
 }
